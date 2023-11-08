6f50d8be25a8e0388052ec09338c23d1168f0653
Nieves <smilenvs@gmail.com>
2023-11-08 18:42:37 +0100

    Intentando meter un menú

--- cliente/gestionveterinario/src/app/app.module.ts
+++ cliente/gestionveterinario/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeRoutedComponent } from './components/shared/home-routed/home-routed.component';
+import { MenubarUnroutedComponent } from './components/shared/menu-unrouted/menu-unrouted/menu-unrouted.component';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-   // AppComponent        Da error
+   AppComponent,
+   HomeRoutedComponent,
+   MenubarUnroutedComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
